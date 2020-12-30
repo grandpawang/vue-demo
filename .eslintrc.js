58c66aec5f168770c3120e8dcd84903b99240d65
grandpawang <907415276@qq.com>
2020-12-30 13:55:46 +0800

    perf: :art: 代码样式

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,33 +5,34 @@ module.exports = {
     sourceType: 'module'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: 'standard',
   plugins: [
     'html'
   ],
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    "no-unused-vars": [2, {
+    'no-unused-vars': [2, {
       // 允许声明未使用变量
-      "vars": "local",
+      vars: 'local',
       // 参数不检查
-      "args": "none"
+      args: 'none'
     }],
     // 关闭语句强制分号结尾
-    "semi": [0],
-    //空行最多不能超过100行
-    "no-multiple-empty-lines": [0, {"max": 100}],
-    //关闭禁止混用tab和空格
-    "no-mixed-spaces-and-tabs": [0],
-    //数组第一个指定是否启用这个规则，第二个指定几个空格
-    "indent":[1,2],
+    semi: [0],
+    // 空行最多不能超过100行
+    'no-multiple-empty-lines': [0, { max: 100 }],
+    // 关闭禁止混用tab和空格
+    'no-mixed-spaces-and-tabs': [0],
+    // 数组第一个指定是否启用这个规则，第二个指定几个空格
+    indent: [1, 2],
+    'new-cap': 'off'
   }
 }
